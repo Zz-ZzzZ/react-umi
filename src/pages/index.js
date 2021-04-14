0100e8aa20a70e4c9910fad230d5619be328f8a8
xgd <1136261471@qq.com>
2021-04-14 17:49:20 +0800

    feat:
    新增路由切换动画

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import NavBar from '../layout/nav-bar/nav-bar';
-import style from './index.less';
 import MenuBar from '../layout/menu-bar/menu-bar';
+import style from './index.less';
+import withRouter from 'umi/withRouter';
+import React from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-const Index = ({ children, location }) => {
+const ANIMATION_MAP = {
+  PUSH: 'forward',
+  POP: 'back',
+};
+
+const Index = withRouter(({ children, location, history }) => {
   return (
     <div className={style.main}>
       <NavBar />
@@ -10,10 +18,21 @@ const Index = ({ children, location }) => {
         <div className={style.containerLeft}>
           <MenuBar location={location} />
         </div>
-        <div className={style.containerRight}>{children ? children : null}</div>
+        <div className={style.containerRight}>
+          <TransitionGroup
+            childFactory={child => {
+              return React.cloneElement(child, { classNames: 'forward' });
+            }}
+            style={{ width: '100%', height: '100%', position: 'relative' }}
+          >
+            <CSSTransition key={location.pathname} timeout={1000}>
+              {children}
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
       </div>
     </div>
   );
-};
+});
 
 export default Index;
